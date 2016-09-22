be797dce235e26479129b5541c2abe0da0ec3e7a
AntonTokarskyi <antontokarskyi@gmail.com>
2016-09-21 22:12:43 -0400

    initial release

---
+++ testGit3/testGit3.cpp
@@ -0,0 +1,13 @@
+// testGit3.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello";
+	return 0;
+}
+
